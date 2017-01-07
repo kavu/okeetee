10271d4d3299648dfc2690b0786528f0655bdb99
Max Riveiro <kavu13@gmail.com>
2017-01-07 22:02:15 +0300

    Raise limits
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -4,8 +4,8 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 # worker_class = "gaiohttp"
 
-max_requests = 100
-max_requests_jitter = 25
+max_requests = 1000
+max_requests_jitter = 100
 
 preload = True
 sendfile = True
